1124a23d588766e1099cf2b09eec3980f46543ea
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-17 00:11:24 +0530

    Update RestaurentDto

--- src/main/java/com/jananga/model/RestaurentDto.java
+++ src/main/java/com/jananga/model/RestaurentDto.java
@@ -1,4 +1,20 @@
 package com.jananga.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
 public class RestaurentDto {
+    private String title;
+
+    @Column
+    private List<String> images;
+
+    private String description;
+    private Long id;
+
 }
